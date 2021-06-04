19c7911ccc6990f56ade01b8e5b8e131b4515d75
anighosh <ani.biogeo@gmail.com>
2021-06-04 12:59:50 +0300

    added historical, common y-axis

--- 1_past_future_summary_plot.R
+++ 1_past_future_summary_plot.R
@@ -2,8 +2,10 @@ library(tidyverse)
 library(cowplot)
 library(fst)
 library(ggthemes)
-library(showtext)
-font_add_google("Merriweather")
+# library(showtext)
+library(extrafont)
+# font_import()
+loadfonts(device = "win")
 
 datadir <- "G:/My Drive/work/ciat/climate_risk_profiles/kenya_counties/data"
 
@@ -47,20 +49,22 @@ d2 <- d2 %>%
     season == "s2" ~ "Season 2"))
 
 d2$era <- factor(d2$era, levels = c('hist', '2030', '2050'))
-yrange <- round(c(min(d2$AMT_mean) - 2 , max(d2$AMT_mean) + 2))
+trange <- round(c(min(d2$AMT_mean) - 2 , max(d2$AMT_mean) + 2))
 
-# past pattern
+font <- "Sans"
+
+# past temeprature pattern
 plot1 <- ggplot(d1, aes(year, AMT_mean, col = season)) +
   geom_line(size = 1) +
   geom_point() +
-  scale_y_continuous(limits = yrange) +
-  labs(title = "Past temperature trends", x = "Year", y = expression("Temperature "(~degree~C))) +
+  scale_y_continuous(limits = trange) +
+  labs(x = "Year", y = expression("Temperature "(~degree~C))) +
   theme_classic() +
-  theme(text = element_text(size = 16, family = "Cambria"),
-    axis.title.y = element_text(size = 12),
-    axis.title.x = element_text(size = 12),
+  theme(text = element_text(size = 20, family = font),
+    axis.title.y = element_text(size = 16),
+    axis.title.x = element_text(size = 16),
     axis.text.x = element_text(size = 12),
-    axis.text.y = element_text(size = 14),
+    axis.text.y = element_text(size = 12),
     legend.text = element_text(size=12),
     legend.title = element_blank(),
     legend.position = c(.01, .99),
@@ -76,16 +80,17 @@ plot1 <- ggplot(d1, aes(year, AMT_mean, col = season)) +
   
 plot2 <- ggplot(d2, aes(x=as.factor(season), y=AMT_mean, fill=era)) +
   geom_bar(position=position_dodge(), stat="identity") +
-  coord_cartesian(ylim = yrange) +
+  coord_cartesian(ylim = trange) +
   geom_errorbar(aes(ymax = AMT_mean + AMT_sd, ymin = AMT_mean - AMT_sd), width=.2, position=position_dodge(.9)) +
-  labs(title = "Temperature", x = "Year", y = expression("Temperature "(~degree~C))) +
+  labs(x = "Year") +
   theme_classic() +
   scale_fill_manual(values = c("#fe9929", "#cc4c02", "#a50f15"), aesthetics = c("color", "fill")) +
-  theme(text = element_text(size = 16, family = "Cambria"),
-        axis.title.y = element_text(size = 12),
-        axis.title.x = element_text(size = 12),
+  theme(text = element_text(size = 16, family = font),
+        axis.title.y = element_blank(),
+        # axis.title.y = element_text(size = 16),
+        axis.title.x = element_text(size = 16),
         axis.text.x = element_text(size = 12),
-        axis.text.y = element_text(size = 14),
+        axis.text.y = element_text(size = 12),
         legend.text = element_text(size=12),
         legend.title = element_blank(),
         legend.position = c(.01, .99),
@@ -94,46 +99,65 @@ plot2 <- ggplot(d2, aes(x=as.factor(season), y=AMT_mean, fill=era)) +
         legend.margin = margin(6, 6, 6, 6),
         legend.direction="horizontal")
 
-pp1 <- plot_grid(plot1, plot2, labels = NULL, rel_widths = c(2, 1.2))
+titletext <- ggdraw() + draw_label("Annual Mean Temperature Trends", fontface='bold', size = 20)
+pp1 <- plot_grid(plot1, plot2, rel_widths = c(2, 1.2), align = "h")
+pp1 <- plot_grid(titletext, pp1, ncol = 1, rel_heights=c(0.1, 1))
 
 save_plot(file.path(datadir, paste0(county, "_temperature_plot.png")), pp1, base_height = 5, base_width = 12)
 
 
 ######################################################################################################
+prange <- round(c(min(d2$ATR_mean-d2$ATR_sd) - 200 , max(d2$ATR_mean+d2$ATR_sd) + 200))
+
+# past temeprature pattern
 plot3 <- ggplot(d1, aes(year, ATR_mean, col = season)) +
   geom_line(size = 1) +
   geom_point() +
-  labs(title = "Past rainfall trends", x = "Year", y = "Annual total rainfall (mm)") +
-  theme_bw() +
-  theme(
-    axis.title.y = element_text(size = 12),
-    axis.title.x = element_text(size = 12),
-    axis.text.x = element_text(size = 12, angle = 90, vjust = 0.5),
-    axis.text.y = element_text(size = 14),
-    legend.text = element_text(size=12),
-    legend.title = element_text(size=14),
-    legend.position = c(.01, .99),
-    legend.justification = c("left", "top"),
-    legend.box.just = "left",
-    legend.margin = margin(6, 6, 6, 6)
+  scale_y_continuous(limits = prange) +
+  labs(x = "Year", y = "Rainfall (mm)") +
+  theme_classic() +
+  theme(text = element_text(size = 20, family = font),
+        axis.title.y = element_text(size = 16),
+        axis.title.x = element_text(size = 16),
+        axis.text.x = element_text(size = 12),
+        axis.text.y = element_text(size = 12),
+        legend.text = element_text(size=12),
+        legend.title = element_blank(),
+        legend.position = c(.01, .99),
+        legend.justification = c("left", "top"),
+        legend.box.just = "left",
+        legend.margin = margin(6, 6, 6, 6)
   ) +
   geom_ribbon(aes(ymax = ATR_mean + ATR_sd, ymin = ATR_mean - ATR_sd, fill = season), alpha = 0.3, colour=NA) +
-  scale_fill_manual(values = c("#253494", "#41b6c4"), aesthetics = c("color", "fill")) + 
-  scale_x_continuous(labels = sort(unique(da$year)), breaks = sort(unique(da$year)))
+  scale_fill_manual(values = c("#a8ddb5", "#2b8cbe"), aesthetics = c("color", "fill")) + 
+  # scale_x_continuous(labels = sort(unique(d1$year)), breaks = sort(unique(d1$year)))
+  scale_x_continuous(breaks = round(seq(min(d1$year), max(d1$year), by = 5), 1))
 
-# future barplots
-plot4 <- ggplot(d2, aes(x=as.factor(era), y=ATR_mean, fill=season)) +
+
+plot4 <- ggplot(d2, aes(x=as.factor(season), y=ATR_mean, fill=era)) +
   geom_bar(position=position_dodge(), stat="identity") +
+  coord_cartesian(ylim = prange) +
   geom_errorbar(aes(ymax = ATR_mean + ATR_sd, ymin = ATR_mean - ATR_sd), width=.2, position=position_dodge(.9)) +
-  labs(title = "Future rainfall", x = "Year", y = "Annual total rainfall (mm)") +
-  theme_bw() +
-  scale_fill_manual(values = c("#253494", "#41b6c4"), aesthetics = c("color", "fill")) +
-  theme(legend.position = "none",
-        axis.title.y = element_text(size = 12),
-        axis.title.x = element_text(size = 12),
-        axis.text.x = element_text(size = 12, angle = 90, vjust = 0.5),
-        axis.text.y = element_text(size = 14)) 
+  labs(x = "Year") +
+  theme_classic() +
+  scale_fill_manual(values = c("#08519c", "#4292c6", "#9ecae1"), aesthetics = c("color", "fill")) +
+  theme(text = element_text(size = 16, family = font),
+        axis.title.y = element_blank(),
+        # axis.title.y = element_text(size = 16),
+        axis.title.x = element_text(size = 16),
+        axis.text.x = element_text(size = 12),
+        axis.text.y = element_text(size = 12),
+        legend.text = element_text(size=12),
+        legend.title = element_blank(),
+        legend.position = c(.01, .99),
+        legend.justification = c("left", "top"),
+        legend.box.just = "left",
+        legend.margin = margin(6, 6, 6, 6),
+        legend.direction="horizontal")
 
-pp2 <- plot_grid(plot3, plot4, labels = NULL, rel_widths = c(2, 1.2))
+raintitle <- ggdraw() + draw_label("Annual Total Rainfall Trends", fontface='bold', size = 20)
+pp2 <- plot_grid(plot3, plot4, rel_widths = c(2, 1.2), align = "h")
+pp2 <- plot_grid(raintitle, pp2, ncol = 1, rel_heights=c(0.1, 1))
 
 save_plot(file.path(datadir, paste0(county, "_rainfall_plot.png")), pp2, base_height = 5, base_width = 12)
+
