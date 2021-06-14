82f0ca196720a83e8e03e4db2fe3964eca03c533
anighosh <ani.biogeo@gmail.com>
2021-06-14 16:12:55 +0300

    more plots

--- 2_prepare_mapdata.R
+++ 2_prepare_mapdata.R
@@ -1,20 +1,16 @@
 # combine data from the two different set of files
 library(raster)
+library(terra)
 library(rasterVis)
 library(fst)
 library(tidyverse)
 library(tmap)
 library(tmaptools)
+library(patchwork)
+library(grid)
+library(fields)
 
-fill.na <- function(x, i=5) {
-  if( is.na(x)[i] ) {
-    return( round(mean(x, na.rm=TRUE),0) )
-  } else {
-    return( round(x[i],0) )
-  }
-} 
-
-
+# read each models
 readfile <- function(x){
   models <- c("ipsl_cm5a_mr","miroc_esm_chem","ncc_noresm1_m","past")
   # mp <- paste0(models, collapse = "|")
@@ -23,85 +19,288 @@ readfile <- function(x){
   return(d)
 }
 
-makeCountyRaster <- function(d, v1, r1){
+
+# fill missing pixels
+fill.na <- function(x, i=5) {
+  if( is.na(x)[i] ) {
+    return(round(mean(x, na.rm=TRUE), 2))
+  } else {
+    return(round(x[i], 2))
+  }
+} 
+
+
+# make high res raster from tables
+# makeCountyRaster <- function(d, v1, r1){
+#   coordinates(d) <- ~x+y
+#   gridded(d) <- TRUE  
+#   rd <- raster(d)
+#   rd <- extend(rd, extent(v1)*1.25, value=NA)
+#   r11 <- focal(rd, w = matrix(1,3,3), fun = fill.na, pad = TRUE, na.rm = FALSE )
+#   
+#   # replace missing pixels
+#   rd <- cover(rd, r11)
+#   rd <- resample(rd, r1)
+#   rd <- mask(rd, v1)
+#   return(rd)
+# }
+
+fill.interpolate <- function(rd, r, modeled = FALSE){
+  xy <- data.frame(xyFromCell(rd, 1:ncell(rd)))
+  vals <- raster::getValues(rd)
+  # add elevation
+  p <- raster::aggregate(r, res(rd)/res(r))
+  
+  # remove NAs
+  set.seed(1)
+  i <- !is.na(vals)
+  xy <- xy[i,]
+  vals <- vals[i]
+  k <- sample(1:nrow(xy), nrow(xy)*0.2)
+  xy <- xy[k,]
+  vals <- vals[k]
+  # z <- raster::extract(p, xy)
+  # xyz <- cbind(xy, z)
+  
+  #### Thin plate spline model
+  tps <- Tps(xy, vals)
+  # use model to predict values at all locations
+  p <- interpolate(r, tps, ext = extent(v))
+  p <- mask(p, r)
+  # p <- resample(p, r)
+  # 
+  # if (modeled){
+  #   pr <- p
+  # } else {
+  #   pr <- cover(rd, p)
+  # }
+  # pr <- crop(pr, v)
+  # pr <- mask(pr, v)
+  return(p)
+}
+
+makeCountryRaster <- function(d, v, r, modeled){
   coordinates(d) <- ~x+y
   gridded(d) <- TRUE  
   rd <- raster(d)
-  rd <- extend(rd, extent(v1)*1.25, value=NA)
-  r11 <- focal(rd, w = matrix(1,3,3), fun = fill.na, pad = TRUE, na.rm = FALSE )
+  # rd <- mask(rd, v)
+  rd <- extend(rd, extent(v), value=NA)
+  # r11 <- focal(rd, w = matrix(1,3,3), fun = fill.na, pad = TRUE, na.rm = FALSE )
+  pr <- fill.interpolate(rd, r, modeled)
   
-  rd <- cover(rd, r11)
-  rd <- resample(rd, r1)
-  rd <- mask(rd, v1)
-  return(rd)
+  if (var %in% c("CDD", "NT35")){
+    pr <- round(pr)
+  }
+  return(pr)
 }
+
+# theme for ggplot
+thememap <- function(...){
+  theme_bw() + 
+  theme(line = element_blank(),
+        panel.spacing.x = unit(2, "lines"),
+        axis.text.x = element_text(angle = 60, hjust = 1),
+        axis.text = element_text(size = 20), 
+        axis.title = element_text(size = 25),
+        strip.text = element_text(size = 30),
+        legend.position = 'bottom', 
+        legend.title = element_text(size = 30),
+        legend.text = element_text(size = 25),
+        legend.spacing = unit(5, units = 'cm'),
+        legend.spacing.x = unit(1.0, 'cm'), plot.title = element_text(hjust = 0.5),
+          ...
+  ) 
+}
+
+# 3 panel map for each time perios
+makeAbsoluteMap <- function(gs, var, vfort, cboundfort, xlims, ylims, ...){
+  
+  # making sure order is correct
+  gs1 <- raster::subset(gs, grep("1985", names(gs)))
+  gs2 <- raster::subset(gs, grep("2021", names(gs)))
+  gs3 <- raster::subset(gs, grep("2041", names(gs)))
+  gs <- raster::stack(gs1, gs2, gs3)
+  names(gs) <- c("b1", "b2", "b3")
+  # color breaks and limits
+  mv <- min(cellStats(gs, 'min', na.rm = T))
+  mx <- max(cellStats(gs, 'max', na.rm = T))          
+  my_limits <- c(round(mv*0.9,0), round(mx*1.1, 0))
+  my_breaks <- round(seq(my_limits[1], my_limits[2],  length.out = 3), 0)
+  my_limits <- c(ifelse(my_limits[1] > my_breaks[1], my_breaks[1], my_limits[1]),
+                 ifelse(my_limits[2] < my_breaks[3], my_breaks[3], my_limits[2]))
+  
+  # setup color palette
+  if(var$vars %in% c("P5D","P95","ATR","SLGP", "LGP")){
+    my_palette <- scale_fill_gradientn(limits =  my_limits, 
+                                breaks = unique(my_breaks),
+                                labels = unique(my_breaks),
+                                colours = blues9, 
+                                na.value = NA,
+                                guide = guide_colourbar(barwidth = 25, 
+                                                        label.theme = element_text(angle = 25, size = 35)))
+  } else if(var$vars == 'AMT'){
+    my_palette <- scale_fill_gradient(limits =  my_limits, 
+                               breaks = unique(my_breaks),
+                               labels = unique(my_breaks),
+                               low = "yellow", high = "red",
+                               na.value = NA,
+                               guide = guide_colourbar(barwidth = 25,  
+                                                       label.theme = element_text(angle = 25, size = 35)))
+  } else {
+    my_palette <- scale_fill_viridis_c(limits = my_limits, 
+                                breaks = unique(my_breaks),
+                                labels = unique(my_breaks),
+                                na.value = NA,
+                                guide = guide_colourbar(barwidth = 20, 
+                                                        label.theme = element_text(angle = 25, size = 35))) 
+  }
+  
+  absmap <- gplot(gs) + geom_tile(aes(fill = value)) +
+    facet_wrap(~ variable, labeller = labeller(variable = 
+                                                 c("b1" = "1985-2015",
+                                                   "b2" = "2021-2040",
+                                                   "b3" = "2041-2060"))) +
+    geom_path(data = vfort, aes(x = long, y = lat, group = group), 
+              color = gray(0.8), size = 1) +
+    geom_path(data = cboundfort, aes(x = long, y = lat, group = group), 
+              color = gray(0.1), size = 2) +
+    coord_sf(xlim = xlims, ylim = ylims) +
+    scale_y_continuous(breaks = round(ylims, 2), n.breaks = 3) +
+    scale_x_continuous(breaks = round(xlims, 2), n.breaks = 3) +
+    thememap() +
+    labs(fill = var$labs, 
+         x = 'Longitude', y = 'Latitude') +
+    my_palette
+  
+  return(absmap)
+}
+
+# 2 panel map for showing changes
+makeChangeMap <- function(gs, var, vfort, cboundfort, xlims, ylims, ...){
+  gs1 <- raster::subset(gs, grep("1985", names(gs)))
+  gs2 <- raster::subset(gs, grep("2021", names(gs)))
+  gs3 <- raster::subset(gs, grep("2041", names(gs)))
+  
+  # compute changes
+  gs21 <- gs2 - gs1
+  gs31 <- gs3 - gs1
+  gs <- stack(gs21, gs31)
+  names(gs) <- c("b1", "b2")
+  
+  # color breaks and limits
+  my_limits_MM <- c(min(cellStats(gs, 'min', na.rm = T)), max(cellStats(gs, 'max', na.rm = T)))
+  my_breaks_MM <- round(seq(my_limits_MM[1], my_limits_MM[2],  length.out= 3), 0)
+  my_limits_MM <- c(ifelse(my_limits_MM[1] > my_breaks_MM[1], my_breaks_MM[1], my_limits_MM[1]),
+                    ifelse(my_limits_MM[2] < my_breaks_MM[3], my_breaks_MM[3], my_limits_MM[2]))
+  
+  # main plot
+  chgmap <- gplot(gs) + geom_tile(aes(fill = value)) +
+    facet_wrap(~ variable, labeller = labeller(variable = 
+                                                 c("b1" = "Change 2021-2040",
+                                                   "b2" = "Change 2041-2060"))) +
+    # if want to include Delta "\u0394 2041-2060"
+    geom_path(data = vfort, aes(x = long, y = lat, group = group), 
+              color = gray(0.8), size = 1) +
+    geom_path(data = cboundfort, aes(x = long, y = lat, group = group), 
+              color = gray(0.1), size = 2) +
+    coord_sf(xlim = xlims, ylim = ylims) +
+    scale_y_continuous(breaks = round(ylims, 2), n.breaks = 3) +
+    scale_x_continuous(breaks = round(xlims, 2), n.breaks = 3) +
+    thememap() +
+    labs(fill = paste0("Change in \n", var$labs), 
+         x = 'Longitude', y = 'Latitude') +
+    scale_fill_gradient2(limits =  my_limits_MM, 
+                         breaks = my_breaks_MM,
+                         na.value = NA,
+                         low = var$gradlow, mid = var$gradmid, high = var$gradhigh, 
+                         guide = guide_colourbar(barwidth = 20, 
+                                                 label.theme = element_text(angle = 25, size = 35)))
+  return(chgmap)
+}
+
+
+#####################################################################################################
+# prepare files to make raster and plots
 
 datadir <- "G:/My Drive/work/ciat/climate_risk_profiles/kenya_counties/data"
 
-r <- getData('alt', country='KEN', path = datadir)
-v <- getData('GADM', country='KEN', level=1, path = datadir)
+# vector boundaries
+# gadm boundary
+vg <- getData('GADM', country='KEN', level=1, path = datadir)
+# IEBC boundary
+ve <- shapefile(file.path(datadir, "ken_adm_iebc_20191031_shp/ken_admbnda_adm1_iebc_20191031.shp"))
+clist <- c("Bungoma","Kiambu","Kirinyaga","Kisii","Kitui","Migori",     
+           "Murang'a","Nandi","Narok","Nyamira","Samburu","Trans Nzoia","Turkana","Vihiga")
+v <- ve[ve$ADM1_EN %in% clist, ]
+# reading global elevation because of disputed areas
+# r <- stack("G:/My Drive/work/ciat/eia/analysis/input/elevation/wc2.1_30s_elev.tif")
+# # crop to Kenya iebc boundary
+# r <- crop(r, extent(v)*1.1, filename = file.path(datadir, "kenya_elevation_1km.tif"), overwrite = TRUE)
+r <- stack(file.path(datadir, "kenya_elevation_1km.tif"))
+r <- crop(r, v)
+elev <- mask(r, v)
+
+# all county files
 ss <- list.files(datadir, pattern = "_special.fst$", full.names = T, recursive = T)
 ff <- list.files(datadir, pattern = "_corrected.fst$", full.names = T, recursive = T)
 
 countylist <- unique(sapply(strsplit(basename(ff), "_"), "[[", 1))
 periods <- c("1985_2015", "2021_2045", "2041_2065")
 
+# model -> IEBC
+# "Trans-Nzoia" -> "Trans Nzoia"
+# "Muranga" -> "Murang'a"
+# s <- grep(county, ss, value = TRUE)
+# f <- grep(county, ff, value = TRUE)
 
-# county functions
-cat("Prepping", county, "\n")
-
-# process spatial data
-cbound <- v[v$NAME_1 == county,]
-celev <- crop(r, cbound)
-
-shp_sf <- cbound %>% sf::st_as_sf()
-country <- v %>% sf::st_as_sf()
-xlims <- sf::st_bbox(shp_sf)[c(1, 3)]
-ylims <- sf::st_bbox(shp_sf)[c(2, 4)]
-
-vfort <- fortify(v)
-cboundfort <- fortify(cbound)
-
-combineInd <- function(county){}
-s <- grep(county, ss, value = TRUE)
-f <- grep(county, ff, value = TRUE)
-
-ds <- lapply(s, readfile)
-ds <- bind_rows(ds)
-
-df <- lapply(f, readfile)
-df <- bind_rows(df)
-
-# merge
-dfs <- merge(df, ds, by = c("year", "season", "id", "model"))
-dfs <- dfs %>%  mutate(era = case_when(
-  year <= 2015 ~ "1985-2015",
-  year > 2015 & year <= 2041 ~ "2021-2040",
-  year > 2040 & year <= 2065 ~ "2041-2060"))
-
-write_fst(dfs, file.path(datadir, "combined_data", paste0(county, "_all_data_combined.fst")))
+# read model output and process for all counties together
+ofile <- file.path(datadir, "combined_data", "kenya_all_data_combined.fst")
+if(!file.exists(ofile)){
+  ds <- lapply(ss, readfile)
+  ds <- bind_rows(ds)
+  
+  df <- lapply(ff, readfile)
+  df <- bind_rows(df)
+  
+  # merge
+  dfs <- merge(df, ds, by = c("year", "season", "id", "model"))
+  dfs <- dfs %>%  mutate(era = case_when(
+    year <= 2015 ~ "1985-2015",
+    year > 2015 & year <= 2041 ~ "2021-2040",
+    year > 2040 & year <= 2065 ~ "2041-2060"))
+  
+  write_fst(dfs, ofile)
+} else {
+  dfs <- read_fst(ofile)
+}
 
 # aggregate observations and save as high resolution raster
 
-dfa <- dfs %>% 
-  dplyr::select(-c(ISO3,Country,county, year, model, gSeason, LGP, SLGP)) %>%
-  group_by(id, x, y, era, season) %>%
-  summarise_all(mean, na.rm = TRUE) %>%
-  ungroup() %>%  
-  mutate(season = case_when(
-    season == "s1" ~ "Long Rain",
-    season == "s2" ~ "Short Rain"))
-          
-gSeason_i <- dfs %>% 
-  dplyr::select(c(id, x, y, year, era, gSeason)) %>%
-  group_by(id, x, y, year, era) %>% 
-  summarise(gSeason = max(gSeason)) %>%
-  ungroup() %>% 
-  group_by(era, id, x, y) %>%
-  summarise(gSeason = round(mean(gSeason, na.rm = TRUE), 0)) %>% ungroup()          
+# except gSeason/SLGP/LGP
+dfs1 <- dfs %>% 
+  dplyr::select(-c(year, ISO3, Country, county, model, gSeason, SLGP, LGP)) %>%
+  group_by(id, x, y, era, season) %>% 
+  summarise_all(mean, na.rm = TRUE) %>% mutate_at(.vars =  vars(CDD, NT35), .funs = function(x){round(x, 0)}) %>% 
+  ungroup() 
 
+write_fst(dfs1, file.path(datadir, "combined_data", "kenya_woGseaon_data_combined.fst"))
 
-two_index <- dfs %>% 
+for (var in c("CDD","P5D","P95","NT35","ndws","ATR","AMT")){
+  for (season in c("s1", "s2")){
+    for (era in c("1985-2015","2021-2040","2041-2060")){
+      cat("processing ", var, era, season, "\n")
+      d <- dfs1[dfs1$season == season & dfs1$era == era, c("x", "y", var)]
+      # d <- dd[dd$season == season & dd$era == era, !colnames(dd) %in% c("id", "era", "season")]
+      pr <- makeCountryRaster(d, v, elev)
+      names(pr) <- paste(var, era, season, sep="_")
+      writeRaster(pr, file.path(datadir, "combined_data/interim", paste0("interpolated_",names(pr), ".tif")), overwrite = TRUE)
+    }
+  }
+}
+
+# gSeason/SLGP/LGP
+# why does it take so much time?
+dfs2 <- dfs %>% 
   dplyr::select(id, x, y, era, gSeason, SLGP, LGP) %>% 
   group_by(id, era, x, y, gSeason) %>% 
   summarise_all(.f = function(x){round(mean(x, na.rm = TRUE), 0)}) %>% 
@@ -112,88 +311,22 @@ two_index <- dfs %>%
   tidyr::unnest() %>% 
   ungroup()
 
-SLGP_dif <- two_index %>% dplyr::select(-LGP)  %>%
-  pivot_wider(names_from = era, values_from = SLGP) %>%
-  mutate(dif1 = `2021-2040`-`1985-2015`,
-         dif2 = `2041-2060`-`1985-2015`)
+write_fst(dfs2, file.path(datadir, "combined_data", "kenya_wGseaon_data_combined.fst"))
 
-gs1 <- list()
-cols <- c("1985-2015","2021-2040","2041-2060","dif1","dif2")
-slgp1 <- SLGP_dif[SLGP_dif$gSeason == 1, ]
-
-for (i in 1:length(cols)){
-  gs1[[i]] <- makeCountyRaster(slgp1[,c("x", "y", cols[i])], cbound, celev)
+for (var in c("SLGP", "LGP")){
+  for (gSeason in c(1, 2)){
+    for (era in c("1985-2015","2021-2040","2041-2060")){
+      cat("processing ", var, era, gSeason, "\n")
+      d <- dfs2[dfs2$gSeason == gSeason & dfs2$era == era, c("x", "y", var)]
+      # d <- dd[dd$season == season & dd$era == era, !colnames(dd) %in% c("id", "era", "season")]
+      pr <- makeCountryRaster(d, v, r, modeled = TRUE)
+      pr[pr > 365] <- 365
+      names(pr) <- paste(var, era, paste0("s",gSeason), sep="_")
+      writeRaster(pr, file.path(datadir, "combined_data/interim", paste0("interpolated_",names(pr), ".tif")), overwrite = TRUE)
+    }
+  }
 }
-gs1 <- stack(gs1)
-# names(gs1) <- paste0("SGLP_", cols)
-
-gs11 <- gs1[[1:3]]
-
-limits_two <- two_index %>% group_by(gSeason) %>%
-  dplyr::select(SLGP, LGP) %>% summarise_all(.funs = c('min', 'max')) %>% 
-  ungroup()
-gS <- unique(two_index$gSeason)
-
-my_limits <- c(limits_two$SLGP_min[i], limits_two$SLGP_max[i])
-my_breaks <- round(seq(my_limits[1], my_limits[2],  length.out= 3), 0)
-my_limits <- c(ifelse(my_limits[1] > my_breaks[1], my_breaks[1], my_limits[1]) ,ifelse(my_limits[2] < my_breaks[3], my_breaks[3], my_limits[2]))
 
 
-
-pfill <- glue::glue('SLGP\n(Day of\nthe year)  ')
-ptitle <- glue::glue('gSeason = {i}')
-
-absmap <- gplot(gs11) + geom_tile(aes(fill = value)) +
-  facet_wrap(~ variable) +
-  geom_path(data = vfot, aes(x = long, y = lat, group = group), 
-              color = gray(0.8), size = 0.1) +
-  geom_path(data = cboundfort, aes(x = long, y = lat, group = group), 
-            color = gray(0.1), size = 0.1) +
-  coord_sf(xlim = xlims, ylim = ylims) +
-  labs(fill = pfill, 
-       title = ptitle,
-       x = 'Longitude', y = 'Latitude') +
-  scale_fill_viridis_c(limits =  my_limits, breaks = my_breaks, na.value = NA,
-                       guide = guide_colourbar(barwidth = 20, label.theme = element_text(angle = 25, size = 35))) +
-  scale_y_continuous(breaks = round(ylims, 2), n.breaks = 3) +
-  scale_x_continuous(breaks = round(xlims, 2), n.breaks = 3) +
-  theme_bw() + 
-  theme(line = element_blank(),
-    legend.position = 'bottom', text = element_text(size=35), 
-        legend.title=element_text(size=35), 
-        legend.spacing = unit(5, units = 'cm'),
-        legend.spacing.x = unit(1.0, 'cm'), plot.title = element_text(hjust = 0.5)) 
-
-
-gs12 <- gs1[[4:5]]
-dt_MM <- slgp1$dif1
-
-my_limits_MM <- c(min(dt_MM)-1, max(dt_MM)+1)
-my_breaks_MM <- round(seq(my_limits_MM[1], my_limits_MM[2],  length.out= 3), 0)
-my_limits_MM <- c(ifelse(my_limits_MM[1] > my_breaks_MM[1], my_breaks_MM[1], my_limits_MM[1]) ,ifelse(my_limits_MM[2] < my_breaks_MM[3], my_breaks_MM[3], my_limits_MM[2]))
-
-
-chgmap <- gplot(gs12) + geom_tile(aes(fill = value)) +
-  facet_wrap(~ variable) +
-  geom_path(data = fortify(v), aes(x = long, y = lat, group = group), 
-            color = gray(0.8), size = 0.1) +
-  geom_path(data = fortify(cbound), aes(x = long, y = lat, group = group), 
-            color = gray(0.1), size = 0.1) +
-  coord_sf(xlim = xlims, ylim = ylims) +
-  labs(fill = pfill, 
-       title = ptitle,
-       x = 'Longitude', y = 'Latitude') +
-  scale_fill_gradient2(limits =  my_limits_MM, 
-                       breaks = my_breaks_MM,
-                       na.value = NA,
-                       low = '#A50026', mid = 'white', high = '#000099', 
-                       guide = guide_colourbar(barwidth = 20, label.theme = element_text(angle = 25, size = 35))) +
-  scale_y_continuous(breaks = round(ylims, 2), n.breaks = 3) +
-  scale_x_continuous(breaks = round(xlims, 2), n.breaks = 3) +
-  theme_bw() + 
-  theme(line = element_blank(),
-        legend.position = 'bottom', text = element_text(size=35), 
-        legend.title=element_text(size=35), 
-        legend.spacing = unit(5, units = 'cm'),
-        legend.spacing.x = unit(1.0, 'cm'), plot.title = element_text(hjust = 0.5)) 
-chgmap
+#########################################################################################################
+# make maps
---
+++ zzz.R
@@ -0,0 +1,180 @@
+# create a list for variable and palette
+varCol <- data.frame(vars = c("CDD","P5D","P95","NT35","ndws","ATR","AMT","SLGP", "LGP"),
+                     gradlow = c("#000099", "#A50026", "#A50026", "#000099", "#000099", 
+                                 "#A50026", "#000099","#A50026", "#A50026"),
+                     gradmid = rep("white", 9),
+                     gradhigh = c("#A50026", "#000099", "#000099", "#A50026", "#A50026", 
+                                  "#000099", "#A50026","#000099", "#000099"),
+                     labs = c("CDD \n(days)", "P5D\n(mm/5 days)", "P95\n(mm/day)", "NT35 \n(days)",
+                              "NDWS\n(days/month)", "ATR\n(mm/year)", "AMT\n(\u00B0C)",
+                              "SLGP\n(Day of\nthe year)", "LGP\n(Day of\nthe year)"))
+
+# stack of all raster by variables
+rr <- list.files(file.path(datadir, "combined_data/interim"), pattern = "interpolated_", full.names = TRUE)
+
+vars <- c("CDD","P5D","P95","NT35","ndws","ATR","AMT", "SLGP", "LGP")
+seasons <- c("s1", "s2")
+gSeasons <- c(1,2)
+
+makePlotCounty <- function(i, varCol, county, season, v, elev, rr, datadir){
+  var <- varCol[i,]
+  cat("Processing", county, "for", var$vars, season, "\n")
+  
+  # process spatial data
+  # county <- case_when(county == "Trans-Nzoia" ~ "Trans Nzoia",
+  #                      county == "Muranga" ~ "Murang'a",
+  #                      TRUE ~ county)
+  # county specific 
+  cbound <- v[v$ADM1_EN == county,]
+  celev <- crop(elev, extent(cbound)*1.1)
+  
+  # check files
+  cdir <- file.path(datadir, "combined_data/interim", county)
+  dir.create(cdir, F, T)
+  ofile <- file.path(cdir, paste0(county, "_", var$vars,"_",season, ".tif"))
+  unlink(ofile)
+  if(!file.exists(ofile)){
+    rv <- grep(paste0("_",var$vars, "_"), rr, value = TRUE)
+    rvs <- grep(paste0("_",season), rv, value = TRUE )
+    gs <- rast(rvs)
+    vc <- vect(cbound)
+    crs(gs) <- crs(vc)
+    gs <- crop(gs, vc, snap = "in")
+    names(gs) <- gsub(".tif","",basename(rvs))
+    gs <- mask(gs, vc, filename = ofile, overwrite = TRUE)
+  } 
+  gs <- stack(ofile)
+  
+  # boundaries
+  xlims <- c(xmin(cbound), xmax(cbound))
+  ylims <- c(ymin(cbound), ymax(cbound))
+  
+  # fortify for ggplot
+  vfort <- fortify(ve)
+  cboundfort <- fortify(cbound)
+  
+  # maps
+  p1 <- makeAbsoluteMap(gs, var, vfort, cboundfort, xlims, ylims)
+  
+  p2 <- makeChangeMap(gs, var, vfort, cboundfort, xlims, ylims) + theme(axis.title.y = element_blank(),  
+                                                                        axis.text.y = element_blank())
+
+  if (var$vars %in% c("SLGP", "LGP")){
+    fseason <- case_when(
+      season == "s1" ~ "gSeason 1",
+      season == "s2" ~ "gSeason 2")
+  } else{
+    fseason <- case_when(
+      season == "s1" ~ "Long Rain",
+      season == "s2" ~ "Short Rain")
+  }
+  
+  # merge 
+  pp <- p1 + p2 + 
+    plot_annotation(title = paste(county, var$vars, "for", fseason),
+                                  caption = 'Alliance of Bioversity and CIAT',
+                                  theme = theme(plot.title = element_text(size = 35))) +
+    plot_layout(ncol = 2, widths = c(0.6, 0.4))
+  
+  pdir <- file.path(datadir, "combined_data/finalplots", county)
+  dir.create(pdir, F, T)
+  
+  ggsave(file.path(pdir, paste0(county,"_", var, "_", fseason, ".png")), pp, device = "png",
+         width = 24, height = 11, units = "in")
+  
+}
+
+varCol <- varCol[varCol$vars %in% c("LGP", "SLGP"),]
+# all plots in for loop
+for(county in clist){{
+  for (season in seasons){
+    for (i in 1:nrow(varCol)){
+      cat("Processing", i, county, season)
+      makePlotCounty(i, varCol, county, season, v, elev, rr, datadir) 
+    }
+  }
+ }
+}
+
+
+
+###############################################################################################################
+# elevation plots
+datadir <- "G:/My Drive/work/ciat/climate_risk_profiles/kenya_counties/data"
+# vector boundaries
+# gadm boundary
+vg <- getData('GADM', country='KEN', level=1, path = datadir)
+# IEBC boundary
+ve <- shapefile(file.path(datadir, "ken_adm_iebc_20191031_shp/ken_admbnda_adm1_iebc_20191031.shp"))
+clist <- c("Bungoma","Kiambu","Kirinyaga","Kisii","Kitui","Migori",     
+           "Murang'a","Nandi","Narok","Nyamira","Samburu","Trans Nzoia","Turkana","Vihiga")
+v <- ve[ve$ADM1_EN %in% clist, ]
+
+# elev
+r <- stack(file.path(datadir, "kenya_elevation_1km.tif"))
+r <- crop(r, v)
+elev <- mask(r, v)
+
+makeElevationPlot <- function(county, v, elev, datadir){
+  # county boundary
+  cat("Processing", county, "\n")
+  cbound <- v[v$ADM1_EN == county,]
+  celev <- crop(elev, cbound)
+  celev <- mask(celev, cbound)
+  
+  # boundaries
+  xlims <- c(xmin(cbound), xmax(cbound))
+  ylims <- c(ymin(cbound), ymax(cbound))
+  
+  # fortify for ggplot
+  vfort <- fortify(ve)
+  cboundfort <- fortify(cbound)
+  
+  # color breaks and limits
+  mv <- min(cellStats(celev, 'min', na.rm = T))
+  mx <- max(cellStats(celev, 'max', na.rm = T))          
+  my_limits <- c(round(mv*0.9,0), round(mx*1.1, 0))
+  my_breaks <- round(seq(my_limits[1], my_limits[2],  length.out = 3), 0)
+  my_limits <- c(ifelse(my_limits[1] > my_breaks[1], my_breaks[1], my_limits[1]),
+                 ifelse(my_limits[2] < my_breaks[3], my_breaks[3], my_limits[2]))
+  
+  
+  emap <- gplot(celev) + geom_tile(aes(fill = value)) +
+    geom_path(data = vfort, aes(x = long, y = lat, group = group), 
+              color = gray(0.8), size = 1) +
+    geom_path(data = cboundfort, aes(x = long, y = lat, group = group), 
+              color = gray(0.1), size = 2) +
+    coord_sf(xlim = xlims, ylim = ylims) +
+    scale_fill_gradientn(colours = rev(terrain.colors(20)),
+                         na.value =  NA,
+                         limits = my_limits, 
+                         breaks = unique(my_breaks),
+                         labels = unique(my_breaks),
+                         guide = guide_colourbar(barwidth = 15, barheight = 3,
+                                                 label.theme = element_text(angle = 25, size = 25))) +
+    scale_y_continuous(breaks = round(ylims, 2), n.breaks = 3) +
+    scale_x_continuous(breaks = round(xlims, 2), n.breaks = 3) +
+    labs(fill = "Elevation \n(m)",x = 'Longitude', y = 'Latitude') + # title = county ,
+    theme_bw() + 
+    theme(line = element_blank(),
+          panel.spacing.x = unit(2, "lines"),
+          axis.text.x = element_text(angle = 60, hjust = 1),
+          axis.text = element_text(size = 20), 
+          axis.title = element_text(size = 25),
+          strip.text = element_text(size = 30),
+          legend.position = 'bottom', 
+          legend.title = element_text(size = 30),
+          legend.text = element_text(size = 25),
+          legend.spacing = unit(5, units = 'cm'),
+          legend.spacing.x = unit(1.0, 'cm'), plot.title = element_text(hjust = 0.1))
+  
+  pdir <- file.path(datadir, "combined_data/finalplots", county)
+  dir.create(pdir, F, T)
+  
+  ggsave(file.path(pdir, paste0(county,"_elevation.png")), emap, device = "png",
+         width = 12, height = 12, units = "in")
+}
+
+for(county in clist){
+  makeElevationPlot(county, v, elev, datadir)
+}
